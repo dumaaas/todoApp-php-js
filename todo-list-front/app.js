cadce865df3ca9a49716562612e5a64b168c5f10
dumaaas <markodumnic8@gmail.com>
2021-01-18 15:28:39 +0100

    Added button for switching from dark to light mode.

--- todo-list-front/app.js
+++ todo-list-front/app.js
@@ -13,6 +13,24 @@ function citajZadatke() {
     });
 }
 
+// Select the button
+const btn = document.querySelector("#chk");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("change", function() {
+    // If the current URL contains "ligh-theme.css"
+    if (theme.getAttribute("href") == "light-style.css") {
+        // ... then switch it to "dark-theme.css"
+        theme.href = "style.css";
+        // Otherwise...
+    } else {
+        // ... switch it to "light-theme.css"
+        theme.href = "light-style.css";
+    }
+});
+
 //funckija za racunanje uspjesnosti zadataka
 //ukoliko je ispod 50% prikazuje se crvenom bojom, ukoliko je iznad zelenom
 //ukoliko nemamo zadataka, ne prikazuje se na stranici
